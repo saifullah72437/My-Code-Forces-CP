3a8b4d5bb1f0d7a1c59f6d95ca205248778af271
Saifullah Khan <73275780+saifullah72437@users.noreply.github.com>
2023-04-08 10:45:03 -0700

    236A_Boy or Girl

---
+++ 236A_Boy or Girl.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+/********************************************************************************************************************************************************************************************************/
+
+vector<int> mergeSortedArrays(int arr1[], int arr2[], int n, int m)
+{
+
+    vector<int> v;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] < arr2[j])
+            v.push_back(arr1[i++]);
+        else if (arr1[i] > arr2[j])
+            v.push_back(arr2[j++]);
+    }
+    while (i < n)
+    {
+        v.push_back(arr1[i++]);
+    }
+    while (j < m)
+    {
+        v.push_back(arr2[j++]);
+    }
+    return v;
+}
+
+void solve()
+{
+    string s;
+    set<char> st;
+    cin >> s;
+    for (auto i : s)
+    {
+        st.insert(i);
+    }
+
+    int siz = st.size();
+    if (siz & 1)
+        cout << "IGNORE HIM!";
+    else
+        cout << "CHAT WITH HER!";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    solve();
+
+    // cout<<38910;
+}
+ 
